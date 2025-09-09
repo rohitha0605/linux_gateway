ff1e9161c23b7fd93f2668e151d919c1e3b692a0
donthireddyrohitha <rohitha.donthireddy@winwinlabs.org>
2025-09-09 10:50:32 -0400

    build: vendor protoc to fix CI (afl/renode jobs)

--- build.rs
+++ build.rs
@@ -1,6 +1,16 @@
 fn main() {
+    // Rebuild when proto changes
     println!("cargo:rerun-if-changed=proto/rpmsg/calc/v1/calc.proto");
+
+    // Use a vendored protoc so CI and dev machines don't need it installed
+    let protoc = protoc_bin_vendored::protoc_bin_path()
+        .expect("vendored protoc");
+    std::env::set_var("PROTOC", protoc);
+
     prost_build::Config::new()
-        .compile_protos(&["proto/rpmsg/calc/v1/calc.proto"], &["proto"])
-        .unwrap();
+        .compile_protos(
+            &["proto/rpmsg/calc/v1/calc.proto"],
+            &["proto"],
+        )
+        .expect("generate prost code");
 }
